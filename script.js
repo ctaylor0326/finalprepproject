bb2beeee2daaffacd6b2dea0e9dcb281a8c95cbb
CahenTaylor <cahentaylor@gmail.com>
2023-03-19 03:22:21 -0600

    addded event listeners

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ el2.addEventListener('mouseover', function handleMouseOver() {
 el2.addEventListener('mouseout', function handleMouseOut() {
     cosmosInfo.style.visibility = 'hidden';
   });
+
+  const el3 = document.getElementById('liatris');
+const liatrisInfo = document.getElementById('liatrisInfo')
+el3.addEventListener('mouseover', function handleMouseOver() {
+    console.log('moused over')
+    liatrisInfo.style.visibility = 'visible';
+})
+el3.addEventListener('mouseout', function handleMouseOut() {
+    liatrisInfo.style.visibility = 'hidden';
+  });
+
+  const el4 = document.getElementById('alyssum');
+  const alyssumInfo = document.getElementById('alyssumInfo')
+  el4.addEventListener('mouseover', function handleMouseOver() {
+      console.log('moused over')
+      alyssumInfo.style.visibility = 'visible';
+  })
+  el4.addEventListener('mouseout', function handleMouseOut() {
+      alyssumInfo.style.visibility = 'hidden';
+    });
+    
