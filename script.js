26c0e5d86eff06c22d01606162e273f52e6f8568
CahenTaylor <cahentaylor@gmail.com>
2023-03-19 03:17:25 -0600

    added over flower p ids and created mouseover/out event for cosmos

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
 
-const el = document.getElementById('peony');
+const el1 = document.getElementById('peony');
 const peonyInfo = document.getElementById('peonyInfo')
-el.addEventListener('mouseover', function handleMouseOver() {
+el1.addEventListener('mouseover', function handleMouseOver() {
     console.log('moused over')
     peonyInfo.style.visibility = 'visible';
 })
-el.addEventListener('mouseout', function handleMouseOut() {
+el1.addEventListener('mouseout', function handleMouseOut() {
     peonyInfo.style.visibility = 'hidden';
+  });
+
+
+  const el2 = document.getElementById('cosmos');
+const cosmosInfo = document.getElementById('cosmosInfo')
+el2.addEventListener('mouseover', function handleMouseOver() {
+    console.log('moused over')
+    cosmosInfo.style.visibility = 'visible';
+})
+el2.addEventListener('mouseout', function handleMouseOut() {
+    cosmosInfo.style.visibility = 'hidden';
   });
